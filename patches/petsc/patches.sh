7f7dd3a9ce0bd0928178ea111e1a393c68eaf6b4
Matteo Franchin <none@none>
2011-12-12 19:29:50 +0000

    This should fix things in Mac-Lion.
    --HG--
    branch : dist

--- patches/petsc/patches.sh
+++ patches/petsc/patches.sh
@@ -1,11 +1,10 @@
 
-FILE="$1/bmake/common/rules.shared.basic"
+FILE="$1/conf/rules"
 
-if [ "x`grep shared_darwin9 $FILE`" == "x" ]; then
+if [ "x`grep shared_darwin7 $FILE`" == "x" ]; then
   echo "Patching $FILE"
   echo >> $FILE
-  echo "shared_darwin9: shared_darwin7" >> $FILE
+  echo "shared_darwin11: shared_darwin7" >> $FILE
   echo >> $FILE
   echo "done"
 fi
-
